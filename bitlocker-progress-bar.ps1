1a9744f34739d46106cde204fd92aac9aa80875e
Aliza Rogers <arogers5.niu.edu>
2025-06-04 09:36:55 -0500

    Finished creating the bitlocker progress bar.

--- bitlocker-progress-bar.ps1
+++ bitlocker-progress-bar.ps1
@@ -1,1 +1,21 @@
 ﻿# checks & displays Bitlocker encryption progress
+
+
+
+do {
+
+    $percent = Get-BitLockerVolume -MountPoint C | Select-Object -ExpandProperty EncryptionPercentage
+
+    $scaled = [int]($percent/2)
+
+    $filledBars = "▮" * $scaled
+    $emptyBars = "▯" * (50 - $scaled)
+
+    $message = "Bitlocker Progress: " + $filledBars + $emptyBars + " (${percent}%)`r"
+
+    Clear-Host
+    Write-Host $message -NoNewline
+
+    Start-Sleep -s 2
+    
+}while ($percent -lt 100)
