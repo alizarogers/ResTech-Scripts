cbd758fea9635aa83e0f2e0fd533433b68870bd8
Aliza Rogers <143752247+alizarogers@users.noreply.github.com>
2025-06-25 12:54:53 -0500

    Added checks for AMD processors.
    Removed -verbose handling. Running the script with the object flag and not catching the retun object will effectively do the same thing.

--- check-hardware.ps1
+++ check-hardware.ps1
@@ -1,4 +1,22 @@
-﻿param (
+﻿<# 
+    Author: Aliza Rogers:)
+    Creation Date: June 2025
+    Last Updated: June 25, 2025
+
+    This script run locally to decide whether a machine is Windows 11 compliant or not. 
+
+    '-object' returns this object, that gives more detailed information
+
+        Storage   : boolean
+        TPM       : boolean
+        Firmware  : boolean
+        Memory    : boolean
+        Processor : boolean
+
+    Note: While it may be possible to install Windows 11 on non-compliant devices, these are Microsoft's requirements.
+#>
+
+param (
     [switch]$object
 )
 #Windows 11 Requirements
@@ -8,7 +26,7 @@ $rqdMemory = 4294967296 #bytes
 $rqdStorage = 68719476736 #bytes
 $rqdFirmware = "UEFI"
  
-function get-specs {
+function Get-Specs {
         
     $systemdrive = Get-WmiObject -Class Win32_OperatingSystem | Select-Object -ExpandProperty SystemDrive
 
@@ -20,10 +38,10 @@ function get-specs {
     
     $results = [PSCustomObject]@{
         Processor = $computerInfo.CsProcessors[0]
-        Memory = $computerInfo.CsTotalPhysicalMemory
-        TPM = $tpm.ManufacturerVersionFull20
-        Storage = $storage.Size
-        Firmware = $computerInfo.BiosFirmwareType
+        Memory    = $computerInfo.CsTotalPhysicalMemory
+        TPM       = $tpm.ManufacturerVersionFull20
+        Storage   = $storage.Size
+        Firmware  = $computerInfo.BiosFirmwareType
     }
 
     return $results
@@ -42,45 +60,62 @@ function Test-ProcessorCompatibility {
     $x86 = 0
 
     if (($processor.Architecture -eq $x64) -or ($processor.Architecture -eq $x86)) {
-        if ($processor.Manufacturer -eq "GenuineIntel") {
-
-            # Find the exceptions below
-            if ($processor.Description -match "Family (\d) Model (\d*)( Stepping )?(\d*)?") {
-                $family = [int]$matches[1]
-                $model = [int]$matches[2]
-                if ($matches.Count -gt 3) { $stepping = [int]$matches[4] } else { $stepping = 0 }
-
+        if ($processor.Description -match "Family (\d) Model (\d*)( Stepping )?(\d*)?") {
+            $family = [int]$matches[1]
+            $model = [int]$matches[2]
+            $stepping = [int]$matches[4]
+        
+            if ($processor.Manufacturer -eq "GenuineIntel") {
+                # Find the Intel exceptions below
                 # if family >= 6 and model <=95, not compatible 
-                # except for family = 6 and model = 85, that is supported
-                if ((($family -ge 6) -and ($model -le 95)) -and (-not ($family -eq 6 -and $model -eq 85))) {
-                    return $false
-
-                    # if family = 6, model is 142, stepping 9, and registry is not 16, not supported
-                    # if family = 6, model is 158, stepping 9, and registry is not 8, not supported
-                }
-                elseif ( $family -eq 6 -and ($model -eq 142 -or $model -eq 158) -and $stepping -eq 9) {
-                    #  $regValue = Invoke-Command -ComputerName $name -ScriptBlock { Get-ItemPropertyValue -Path HKLM:\HARDWARE\DESCRIPTION\System\CentralProcessor\0 -Name "Platform Specific Field 1" }
-                    $regValue = $(Get-ItemPropertyValue -Path HKLM:\HARDWARE\DESCRIPTION\System\CentralProcessor\0 -Name "Platform Specific Field 1") 
-                    if ((($model -eq 142) -and ($regValue -ne 16)) -or (($model -eq 158) -and ($regValue -ne 8))) {
+                if (($family -ge 6) -and ($model -le 95)) {
+                    if ($family -eq 6 -and $model -eq 85) {
+                        # except for family = 6 and model = 85, that is supported
+                        return $true
+                    }
+                    else {
                         return $false
                     }
                 }
+                # if family = 6, model is 142, stepping 9, and registry is not 16, not supported
+                # if family = 6, model is 158, stepping 9, and registry is not 8, not supported
+                elseif ( $family -eq 6 -and ($model -eq 142 -or $model -eq 158) -and $stepping -eq 9) {
+                    $regValue = $(Get-ItemPropertyValue -Path HKLM:\HARDWARE\DESCRIPTION\System\CentralProcessor\0 -Name "Platform Specific Field 1") 
+                    if (($model -eq 142 -and $regValue -ne 16) -or ($model -eq 158 -and $regValue -ne 8)) {
+                        return $false
+                    }
+                    else {
+                        return $true
+                    }
+                }
+                else {
+                    # all other Intel processors are supported
+                    return $true
+                }
             }
-        }
-        elseif ($processor.Manufacturer -eq "AuthenticAMD") {
+            elseif ($processor.Manufacturer -eq "AuthenticAMD") {
          
-            # come back to this later
-            # everything except for family < 23 or (cpu = 23 & model 1 or 17)
+                # everything except for family < 23 or (family = 23 & model 1 or 17)  
+                if ($family -lt 23 -or ($family -eq 23 -and ($model -eq 1 -or $model -eq 17))) {
+                    return $false
+                }
+                else {
+                    return $true
+                }  
+            }
+            else {
+                # if not Intel or AMD
+                return $false
+            }
         }
     }
     else {
-        # If not x86 or 64, assume no
+        # If not x86 or 64
         return $false
     }
 } 
 
 $specs = get-specs
-
 
 $processorMeetsReq = ($specs.Processor.MaxClockSpeed -ge $rqdProcessorSpeed) -and ($specs.Processor.NumberofCores -ge $rqdCores)
 $processorCompatible = Test-ProcessorCompatibility -processor $specs.Processor
@@ -89,19 +124,16 @@ $tpmMeetsReq = -not ($specs.TPM -match "Not Supported")
 $storeageMeetReq = $specs.Storage -ge $rqdStorage
 $firmwareMeetReq = $specs.Firmware -match $rqdFirmware
 
-$requirements = [PSCustomObject]@{
-    Storage = $storeageMeetReq
-    TPM = $tpmMeetsReq
-    Firmware = $firmwareMeetReq
-    Memory = $memoryMeetsReq
-    Processor = $processorMeetsReq -and $processorCompatible
+    
+if ($object) {
+    return [PSCustomObject]@{
+        Storage   = $storeageMeetReq
+        TPM       = $tpmMeetsReq
+        Firmware  = $firmwareMeetReq
+        Memory    = $memoryMeetsReq
+        Processor = $processorMeetsReq -and $processorCompatible
+    }
 }
-
-$requirements | Out-String | Write-Verbose -Verbose
-
-if ($object)
-{
-    return $object
-} else {
-    Write-Host $($processorMeetsReq -and $processorCompatible -and $memoryMeetsReq -and $tpmMeetsReq -and $storeageMeetReq -and $firmwareMeetReq)
+else {
+    return $($processorMeetsReq -and $processorCompatible -and $memoryMeetsReq -and $tpmMeetsReq -and $storeageMeetReq -and $firmwareMeetReq)
 }
