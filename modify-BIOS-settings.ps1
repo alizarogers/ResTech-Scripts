dd8c4c34ac8e89c2f5719c1e137e55ae76da258c
Aliza Rogers <143752247+alizarogers@users.noreply.github.com>
2025-07-02 16:39:22 -0500

    Added error handling in Confirm-Module, Set-AssetTag, and Enable-SecureBoot.

--- modify-BIOS-settings.ps1
+++ modify-BIOS-settings.ps1
@@ -1,4 +1,5 @@
-function Confirm-Module {
+function Confirm-Module { # [untested] Returns whether the operation was successful or not. 
+    # This function must be ran with administrator privileges. 
     param ( $moduleName = "DellBIOSProvider" )
 
     # if the module is not installed, install it
@@ -10,32 +11,60 @@ function Confirm-Module {
             Return $false
         }
     }
-    Import-Module DellBIOSProvider
+    
+    try {
+        Import-Module DellBIOSProvider -ErrorAction Stop # adding this flag, since try-catch only works on terminating errors
+    } catch {
+        Write-Error "There was an error while importing $modulename."
+        Return $false
+    }
+
+    # in case import-module doesn't throw an error, but didn't create the DellSmbios drive
+    try {
+        ls DellSmbios:/
+    } catch {
+        Write-Error "There was an issue with accessing the DellSMbios drive."
+        Return $false
+    }
+
     Return $true
 }
 
-function Set-AssetTag {
+function Set-AssetTag { # [untested] Returns whether the operation was successful or not. 
+
     param ( [Parameter(Mandatory=$true)] $assetTag )
 
-    if (Confirm-Module) {
+    if ( -not (Confirm-Module)) {
+        Write-Error "An error occured while installing the Dell BIOS Powershell module. (Necessary for enabling secure boot.)"
+        Return $false
+    }
+
+    try {
+        Set-Item DellSmbios:\SystemInformation\Asset $assetTag
+    } catch {
+        Write-Error "There was an error while trying to set the asset tag."
+        Return $false
+    }
+
+    Return $true
+}
+
+function Enable-SecureBoot { # [untested] Returns whether the operation was successful or not. 
+
+    if ( -not (Confirm-Module)) {
+        Write-Error "An error occured while installing the Dell BIOS Powershell module. (Necessary for enabling secure boot.)"
+        Return $false
+    }
+
+    # confirm that legacy option ROMs option is disabled (necesarry to turn secure boot on)
+    Set-Location DellSmbios:\AdvancedBootOptions\
+    if ((Get-Item .\LegacyOrom).CurrentValue -eq "Enabled") {
         try {
-            Set-Item DellSmbios:\SystemInformation\Asset $assetTag
-        } catch {
-            Write-Error "There was an error while trying to set the asset tag."
-            Return
-        }
-    }
-}
-
-function Set-SecureBoot {
-    
-    if (Confirm-Module) {
-        # confirm that legacy option ROMs option is disabled (necesarry to turn secure boot on)
-        Set-Location DellSmbios:\AdvancedBootOptions\
-       if ((Get-Item .\LegacyOrom).CurrentValue -eq "Enabled")
-       {
             Set-Item DellSmbios:\AdvancedBootOptions\LegacyOrom "Disabled"
-       }
-
+        } catch {
+            Write-Error "An error occured while disabling legacy option ROMs. (Necessary for enabling secure boot.)"
+            Return $false
+        }  
     }
 }
+
